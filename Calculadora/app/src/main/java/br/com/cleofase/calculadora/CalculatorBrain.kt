a8be2f6669c9780bdafc7f2d6e074e64dc4cb65f
Cleofas Pereira <cleofase@gmail.com>
2018-03-28 23:41:05 -0300

    add calculator engine

---
+++ Calculadora/app/src/main/java/br/com/cleofase/calculadora/CalculatorBrain.kt
@@ -0,0 +1,61 @@
+package br.com.cleofase.calculadora
+
+/**
+ * Created by cleofase on 27/03/18.
+ */
+
+sealed class Operation
+class Constant(val algorithm: Double): Operation()
+class Unary(val algorithm: (Double) -> Double): Operation()
+class Binary(val algorithm: (Double, Double) -> Double): Operation()
+class Equals(): Operation()
+class Reset(): Operation()
+
+class PendingBinaryOperation(val operation: (Double, Double) -> Double, val firstOperand: Double) {
+    fun perform(secondOperand: Double): Double = operation(firstOperand,secondOperand)
+}
+
+class CalculatorBrain {
+
+    private var accumulator: Double = 0.0
+    private val operations: HashMap<String,Operation> = hashMapOf(
+            "+" to Binary({a,b -> a+b}),
+            "-" to Binary({a,b -> a-b}),
+            "x" to Binary({a,b -> a-b}),
+            "/" to Binary({a,b -> a-b}),
+            "%" to Unary({ a -> a/100}),
+            "=" to Equals(),
+            "AC" to Reset()
+            )
+    private var pendingBinaryOperation: PendingBinaryOperation? = null
+
+
+    fun setOperand(operand: Double) {
+        accumulator = operand
+    }
+
+    fun performOperation(operationSymbol: String) {
+        val operation = operations[operationSymbol]
+        when(operation){
+            is Constant -> accumulator = operation.algorithm
+            is Unary -> accumulator = operation.algorithm(accumulator)
+            is Binary -> pendingBinaryOperation = PendingBinaryOperation((operation.algorithm),accumulator)
+            is Equals -> performPendingOperation()
+            is Reset -> reset()
+        }
+    }
+
+    private fun performPendingOperation() {
+        accumulator = if (pendingBinaryOperation != null) {pendingBinaryOperation!!.perform(accumulator)} else {accumulator}
+    }
+
+
+    private fun reset() {
+        accumulator = 0.0
+        pendingBinaryOperation = null
+    }
+
+    val result: Double
+        get() = accumulator
+
+}
--- Calculadora/app/src/main/java/br/com/cleofase/calculadora/MainActivity.kt
+++ Calculadora/app/src/main/java/br/com/cleofase/calculadora/MainActivity.kt
@@ -2,11 +2,67 @@ package br.com.cleofase.calculadora
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var userIsInMiddleOfTyping = false
+    private var userTouchedDot = false
+    private var calculatorEngine = CalculatorBrain()
+    private var displayValue: Double
+        get() = textViewDisplay.text.toString().toDouble()
+        set(newValue) {
+            textViewDisplay.text = newValue.toString()
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textViewZero.setOnClickListener { onCliqueDigit(textViewZero.text.toString()) }
+        textViewOne.setOnClickListener { onCliqueDigit(textViewOne.text.toString()) }
+        textViewTwo.setOnClickListener { onCliqueDigit(textViewTwo.text.toString()) }
+        textViewThree.setOnClickListener { onCliqueDigit(textViewThree.text.toString()) }
+        textViewFour.setOnClickListener { onCliqueDigit(textViewFour.text.toString()) }
+        textViewFive.setOnClickListener { onCliqueDigit(textViewFive.text.toString()) }
+        textViewSix.setOnClickListener { onCliqueDigit(textViewSix.text.toString()) }
+        textViewSeven.setOnClickListener { onCliqueDigit(textViewSeven.text.toString()) }
+        textViewEight.setOnClickListener { onCliqueDigit(textViewEight.text.toString()) }
+        textViewNine.setOnClickListener { onCliqueDigit(textViewNine.text.toString()) }
+        textViewDecimal.setOnClickListener { onCliqueDecimalDot(textViewDecimal.text.toString()) }
+        textViewSum.setOnClickListener { onCliqueOperation(textViewSum.text.toString()) }
+        textViewSubtract.setOnClickListener { onCliqueOperation(textViewSubtract.text.toString()) }
+        textViewMultiply.setOnClickListener { onCliqueOperation(textViewMultiply.text.toString()) }
+        textViewDivide.setOnClickListener { onCliqueOperation(textViewDivide.text.toString()) }
+        textViewPercent.setOnClickListener { onCliqueOperation(textViewPercent.text.toString()) }
+        textViewResult.setOnClickListener { onCliqueOperation(textViewResult.text.toString()) }
+        textViewClear.setOnClickListener { onCliqueOperation(textViewClear.text.toString()) }
+        textViewDel.setOnClickListener { onCliqueKey(textViewDel.text.toString()) }
+    }
+
+    private fun onCliqueKey(symbol: String) {
+        Toast.makeText(this,symbol,Toast.LENGTH_SHORT).show()
+    }
+
+    private fun onCliqueDigit(digit: String) {
+        val currentValue = textViewDisplay.text.toString()
+        textViewDisplay.text = if (userIsInMiddleOfTyping) {currentValue + digit} else {userIsInMiddleOfTyping = true; digit}
+    }
+
+    private fun onCliqueDecimalDot(symbol: String) {
+        if (!userTouchedDot) {
+            val currentValue = textViewDisplay.text.toString()
+            userTouchedDot = true
+            userIsInMiddleOfTyping = true
+            textViewDisplay.text = currentValue + symbol
+        }
+    }
+
+    private fun onCliqueOperation(symbol: String) {
+        calculatorEngine.setOperand(displayValue)
+        userIsInMiddleOfTyping = false
+        userTouchedDot = false
+        calculatorEngine.performOperation(symbol)
+        displayValue = calculatorEngine.result
     }
 }
